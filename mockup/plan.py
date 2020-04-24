86a3aae2230e775c802aebb0c45ac080de461251
alonzo <alonzo6546@outlook.com>
2020-04-23 20:27:12 -0400

    short_account_name test

---
+++ mockup/plan.py
@@ -0,0 +1,4 @@
+Codentake plan
+
+description: The goal of Codentake is to for people to ideas to display their ideas and give others a chance to show them how they would do it.
+
--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import logo from './logo.svg';
 import Navbar from './components/navbar'
 import Display from './components/projectdisplay'
 import './styles.css';
+import Signup, {Login} from './components/enter'
 
 function App() {
   return (
     <div className="App container">
       <header className="App-header">
+        <Login />
+        {/*<Signup />
         <Navbar />
-        <Display />
+        <Display />*/}
       </header>
     </div>
   );
