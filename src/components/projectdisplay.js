15ec10332f4c3856b2c7dde92d791fa65ae1b6ac
glowtoad123 <alonzo6546@alonzo6546>
2020-06-22 20:38:00 -0400

    made it possible to edit projects and to remove tags when editing project

--- src/components/projectdisplay.js
+++ src/components/projectdisplay.js
@@ -1,23 +1,13 @@
 import React, {useState} from 'react'
+import Updateproject from './updateProject'
 import me from './me.jpg'
 import faunadb, { query as q } from "faunadb"
 import edit from './edit.svg'
 
 function Display(){
-//test to see if the project is broken or not
     const [projectArray, setProjectArray] =  useState("")
     const testArray = []
     var serverClient = new faunadb.Client({ secret: 'fnADpgTNT1ACEiUC4G_M5eNjnIPvv_eL99-n5nhe' });
-
-    /*serverClient.query(
-        q.Map(
-            q.Paginate(q.Match(q.Index("projects"))),
-            q.Lambda("X", q.Get(q.Var("X")))
-          )
-    ).then((ret, index) => {ret.data.map(one => {console.log(one.data); testArray.push(one.data)})}).then(
-    //projectArray.map(info => console.log(info))
-    console.log(testArray[4]))*/
-
     const [page, setPage] = useState(false)
     const [chosenOne, setChosenOne] = useState("nothing")
 
@@ -45,28 +35,6 @@ function Display(){
                 borderRadius: '6px'}}><strong>{props.tag}</strong></p>
         )
     }
-
-
-    /*function Displayprop(props){
-
-        const tagList = ['crossplatform', 'desktop', 'react-based']
-
-        return(
-            <div className="display" style={{width: '300px'}}>
-                <center></center>
-                <h1 onClick={choseOne} className="displaytitle"><strong>{props.title}</strong></h1>
-                <p><strong>{props.description}</strong></p>
-                <br />
-                <p style={{display: 'inline-block', margin: '5px'}}>{props.likes}</p>
-                <p style={{display: 'inline-block', margin: '5px'}}>{props.participants}</p>
-                <br />
-                <img className="creatorpic" src={props.pic} />
-                <p className="creatorname"><strong>{props.name}</strong></p>
-                <br />
-                <Tag tag={props.tag}/>
-            </div>
-        )
-    }*/
 
 
     serverClient.query(
@@ -102,20 +70,31 @@ function Display(){
         )
     }
 
+    const [refid, setrefid] = useState("")
+
+    function setRef(event){
+        
+        serverClient.query(
+            q.Get(
+            q.Match(q.Index("Project_Title"), event.target.name)
+        )).then((ret, index) => {setrefid(ret.ref); console.log("ref: " + ret.ref.id); sessionStorage.setItem("ref", ret.ref.id)})
+    }
+
     const username = sessionStorage.getItem("username")
     console.log(username)
+    console.log("refid: " + refid)
 
     function Userdisplay(props){
         return(
             <div className="display" style={{width: '300px',}}>
                 <h1 onClick={choseOne} className="displaytitle"><strong>{props.Project_Title}</strong></h1>
-                {props.Creator === username && <img style={{
+                {props.Creator === username && <img id={props.Id} onClick={setRef} style={{
                 width: '48px', 
                 height: '48px',
                 marginRight: '20px',
                 position: 'relative',
                 left: "110px"
-                }} title={props.description} className="navpic" src={edit} />}
+                }} title={props.description} name={props.Project_Title} className="navpic" src={edit} />}
                 <p style={{backgroundColor: "#ffffff"}}><strong>{props.Description}</strong></p>
                 <br />
                 <h1 className="textHead"><strong>Roadmap</strong></h1>
@@ -135,15 +114,15 @@ function Display(){
     }
 
     return(
-        (chosenOne === "nothing") ? <Displayprop /> : <Userdisplay 
+        chosenOne === "nothing" ? <Displayprop /> : chosenOne !== "nothing" && refid === "" ? <Userdisplay 
         Project_Title= {chosenOne.Project_Title}
         Description= {chosenOne.Description}
         Roadmap={chosenOne.Roadmap}
         Changes={chosenOne.Changes}
         Creator={chosenOne.Creator}
         Categories={chosenOne.Categories}
-        />
-         
+        Id={chosenOne.Id}
+        /> : <Updateproject />
     )
 }
 
